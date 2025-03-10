b681b634c9b21bcfe56c22f332c22d5258268908
Sercan KURBAN <sercankurban1996@gmail.com>
2025-03-11 00:16:02 +0300

    Excel Test

---
+++ Proje20_ExcelTest/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Proje20_ExcelTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvVeriler = new System.Windows.Forms.DataGridView();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSaat = new System.Windows.Forms.TextBox();
+            this.txtDersAdi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVeriler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvVeriler
+            // 
+            this.dgvVeriler.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvVeriler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVeriler.Location = new System.Drawing.Point(12, 12);
+            this.dgvVeriler.Name = "dgvVeriler";
+            this.dgvVeriler.RowHeadersWidth = 51;
+            this.dgvVeriler.RowTemplate.Height = 24;
+            this.dgvVeriler.Size = new System.Drawing.Size(782, 390);
+            this.dgvVeriler.TabIndex = 0;
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(801, 12);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(325, 53);
+            this.btnListele.TabIndex = 1;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(801, 71);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(325, 53);
+            this.btnKaydet.TabIndex = 2;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(832, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Saat:";
+            // 
+            // txtSaat
+            // 
+            this.txtSaat.Location = new System.Drawing.Point(884, 142);
+            this.txtSaat.Name = "txtSaat";
+            this.txtSaat.Size = new System.Drawing.Size(242, 30);
+            this.txtSaat.TabIndex = 4;
+            // 
+            // txtDersAdi
+            // 
+            this.txtDersAdi.Location = new System.Drawing.Point(884, 179);
+            this.txtDersAdi.Name = "txtDersAdi";
+            this.txtDersAdi.Size = new System.Drawing.Size(242, 30);
+            this.txtDersAdi.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(800, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ders Adı:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1138, 414);
+            this.Controls.Add(this.txtDersAdi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSaat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.dgvVeriler);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVeriler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvVeriler;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSaat;
+        private System.Windows.Forms.TextBox txtDersAdi;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Proje20_ExcelTest/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Proje20_ExcelTest
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\beste\\source\\repos\\CSharp25Proje\\Proje20_ExcelTest\\proje20.xlsx;Extended Properties='Excel 12.0 Xml;HDR=YES'");
+        //
+        void listele()
+        {
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from [Sayfa1$]", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvVeriler.DataSource = dt;
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("insert into [Sayfa1$] (Saat,Ders) values (@p1,@p2)",baglanti);
+            komut.Parameters.AddWithValue("@p1", txtSaat.Text);
+            komut.Parameters.AddWithValue("@p2", txtDersAdi.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Yeni Ders Bilgisi Eklendi");
+            listele();
+        }
+    }
+}

896aedbdc3550f413befd517608487bf36d41555
Sercan KURBAN <sercankurban1996@gmail.com>
2025-03-12 21:33:35 +0300

    RSS Haber Başlıkları

---
+++ Proje22_RssTest/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Proje22_RssTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnHurriyet = new System.Windows.Forms.Button();
+            this.lstBasliklar = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnMilliyet = new System.Windows.Forms.Button();
+            this.btnFotomac = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnHurriyet
+            // 
+            this.btnHurriyet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHurriyet.Location = new System.Drawing.Point(10, 604);
+            this.btnHurriyet.Name = "btnHurriyet";
+            this.btnHurriyet.Size = new System.Drawing.Size(230, 46);
+            this.btnHurriyet.TabIndex = 0;
+            this.btnHurriyet.Text = "Hürriyet";
+            this.btnHurriyet.UseVisualStyleBackColor = true;
+            this.btnHurriyet.Click += new System.EventHandler(this.btnHurriyet_Click);
+            // 
+            // lstBasliklar
+            // 
+            this.lstBasliklar.FormattingEnabled = true;
+            this.lstBasliklar.ItemHeight = 24;
+            this.lstBasliklar.Location = new System.Drawing.Point(10, 12);
+            this.lstBasliklar.Name = "lstBasliklar";
+            this.lstBasliklar.Size = new System.Drawing.Size(830, 460);
+            this.lstBasliklar.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 478);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnMilliyet
+            // 
+            this.btnMilliyet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMilliyet.Location = new System.Drawing.Point(310, 604);
+            this.btnMilliyet.Name = "btnMilliyet";
+            this.btnMilliyet.Size = new System.Drawing.Size(230, 46);
+            this.btnMilliyet.TabIndex = 3;
+            this.btnMilliyet.Text = "Milliyet";
+            this.btnMilliyet.UseVisualStyleBackColor = true;
+            this.btnMilliyet.Click += new System.EventHandler(this.btnMilliyet_Click);
+            // 
+            // btnFotomac
+            // 
+            this.btnFotomac.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFotomac.Location = new System.Drawing.Point(610, 604);
+            this.btnFotomac.Name = "btnFotomac";
+            this.btnFotomac.Size = new System.Drawing.Size(230, 46);
+            this.btnFotomac.TabIndex = 4;
+            this.btnFotomac.Text = "Fotomaç";
+            this.btnFotomac.UseVisualStyleBackColor = true;
+            this.btnFotomac.Click += new System.EventHandler(this.btnFotomac_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(308, 478);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(230, 120);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(610, 478);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(230, 120);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(848, 659);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnFotomac);
+            this.Controls.Add(this.btnMilliyet);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lstBasliklar);
+            this.Controls.Add(this.btnHurriyet);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RSS Haber Başlıkları";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHurriyet;
+        private System.Windows.Forms.ListBox lstBasliklar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnMilliyet;
+        private System.Windows.Forms.Button btnFotomac;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Proje22_RssTest/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Proje22_RssTest
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void RSSBasliklariGetir(string link)
+        {
+            lstBasliklar.Items.Clear();
+            XmlTextReader xmloku = new XmlTextReader(link);
+            while (xmloku.Read())
+            {
+                if (xmloku.Name == "title")
+                {
+                    lstBasliklar.Items.Add(xmloku.ReadString());
+                }
+            }
+        }
+
+        private void btnHurriyet_Click(object sender, EventArgs e)
+        {
+            //lstBasliklar.Items.Clear();
+            //XmlTextReader xmloku = new XmlTextReader("https://www.hurriyet.com.tr/rss/anasayfa");
+            //while (xmloku.Read())
+            //{
+            //    if (xmloku.Name == "title")
+            //    {
+            //        lstBasliklar.Items.Add(xmloku.ReadString());
+            //    }
+            //}
+
+            RSSBasliklariGetir("https://www.hurriyet.com.tr/rss/anasayfa");
+        }
+
+        private void btnMilliyet_Click(object sender, EventArgs e)
+        {
+            //lstBasliklar.Items.Clear();
+            //XmlTextReader xmloku = new XmlTextReader("https://www.milliyet.com.tr/rss/rssnew/gundem.xml");
+            //while (xmloku.Read())
+            //{
+            //    if (xmloku.Name == "title")
+            //    {
+            //        lstBasliklar.Items.Add(xmloku.ReadString());
+            //    }
+            //}
+
+            RSSBasliklariGetir("https://www.milliyet.com.tr/rss/rssnew/gundem.xml");
+        }
+
+        private void btnFotomac_Click(object sender, EventArgs e)
+        {
+            //lstBasliklar.Items.Clear();
+            //XmlTextReader xmloku = new XmlTextReader("https://www.fotomac.com.tr/rss/anasayfa.xml");
+            //while (xmloku.Read())
+            //{
+            //    if (xmloku.Name == "title")
+            //    {
+            //        lstBasliklar.Items.Add(xmloku.ReadString());
+            //    }
+            //}
+
+            RSSBasliklariGetir("https://www.fotomac.com.tr/rss/anasayfa.xml");
+        }
+    }
+}

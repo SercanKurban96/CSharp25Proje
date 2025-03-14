e0e0a5f7f19743fece1a9ed24f68138ef2a7b443
Sercan KURBAN <sercankurban1996@gmail.com>
2025-03-14 20:35:13 +0300

    Final

---
+++ Proje25_Final/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Proje25_Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFinal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFinal
+            // 
+            this.btnFinal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFinal.Font = new System.Drawing.Font("Monotype Corsiva", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnFinal.Location = new System.Drawing.Point(90, 91);
+            this.btnFinal.Name = "btnFinal";
+            this.btnFinal.Size = new System.Drawing.Size(237, 127);
+            this.btnFinal.TabIndex = 0;
+            this.btnFinal.Text = "Final";
+            this.btnFinal.UseVisualStyleBackColor = true;
+            this.btnFinal.Click += new System.EventHandler(this.btnFinal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(430, 317);
+            this.Controls.Add(this.btnFinal);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Teşekkürler";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFinal;
+    }
+}
+
---
+++ Proje25_Final/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje25_Final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFinal_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu eğitim bana gerçekten çok şey kattı. Daha önceden C# ve SQL bilgilerim sayesinde Öğrenci Sınav Not Kayıt Sistemi projesinden başlayarak finale kadar geldiğim bu müfredattaki tüm projeleri uygulayarak bilgi birikimimi daha da arttırmış oldum. Eğitim boyunca vermiş olduğu ödevlerle daha da pekiştirmemi sağladığı için Sayın Murat Yücedağ'a çok teşekkür ederim.", "TEŞEKKÜRLER", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
